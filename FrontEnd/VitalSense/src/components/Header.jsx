1d5eb090e6b2275a29f85c96c3109e7e7ad6e9c8
Edward <eddy.the.professor@gmail.com>
2024-05-04 13:27:02 -0400

    Fixed logo

--- FrontEnd/VitalSense/src/components/Header.jsx
+++ FrontEnd/VitalSense/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './HeaderStyle.css';
-import headerImage from '../assets/Untitled.svg';
+import headerImage from '../assets/Logo.svg';
 
 function Header() {
     return (
