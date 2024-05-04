80bf67b6048f7d9abd51e96faa9e575eb3943fdd
Shawna Halladeen <shawnakatrina.halladeen@gmail.com>
2024-05-04 10:50:11 -0400

    Editing main page

--- FrontEnd/VitalSense/src/App.jsx
+++ FrontEnd/VitalSense/src/App.jsx
@@ -3,24 +3,24 @@ import Header from "./components/Header.jsx";
 import Mainbody from "./components/mainbody.jsx"
 import CallButton from "./components/CallButton.jsx"
 import HeartBeat from './components/HeartBeat.jsx';
+
 import './App.css'
 
 function App() {
-
   return (
-    <>
-    <div className="app-container">
-      <div className="header">
-        <Header/>
+      <div className="app-container">
+        <div className="header">
+          <Header/>
+        </div>
+        <div className="content-container">
+          <div className="main-content">
+            <Mainbody/>
+          </div>
+          <div className="heart-beat">
+            <HeartBeat/>
+          </div>
+        </div>
       </div>
-    <div className="main-content">
-      <Mainbody/>
-    </div>
-  </div>
-  <div className="app-container">
-    <HeartBeat/>
-  </div>
-  </>
   );
 }
 
