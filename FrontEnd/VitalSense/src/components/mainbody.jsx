9e1ada31872aa4a66f00adba0ed7ffcbc3175b5d
Shawna Halladeen <shawnakatrina.halladeen@gmail.com>
2024-05-04 23:11:17 -0400

    fixed up the spacing

--- FrontEnd/VitalSense/src/components/mainbody.jsx
+++ FrontEnd/VitalSense/src/components/mainbody.jsx
@@ -24,8 +24,14 @@ function Mainbody() {
         </label>
         <label className="form-label">
           Gender:
-          <input type="text" name="gender" placeholder="Please enter your gender" className="input-field" value={gender} onChange={(event) => {
+          <input type="text" name="gender" placeholder="Enter gender" className="input-field" value={gender} onChange={(event) => {
             setGender(event.target.value)
+          }}/>
+        </label>
+        <label className="form-label">
+          Location:
+          <input type="text" name="location" placeholder="Enter location" className="input-field" value={location} onChange={(event) => {
+            setLocation(event.target.value)
           }}/>
         </label>
         <label className="form-label">
