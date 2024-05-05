fcf3cc0d512ebee4a0990f1ba8ee3df205966448
Shawna Halladeen <shawnakatrina.halladeen@gmail.com>
2024-05-05 05:49:04 -0400

    Added call button

--- FrontEnd/VitalSense/src/components/mainbody.jsx
+++ FrontEnd/VitalSense/src/components/mainbody.jsx
@@ -25,10 +25,13 @@ function Mainbody() {
     setAge(event.target.value)}}/>
         </label>
         <label className="form-label">
-          Gender:
-          <input type="text" name="gender" placeholder="Please enter your gender" className="input-field" value={gender} onChange={(event) => {
-            setGender(event.target.value)
-          }}/>
+        
+          <div>
+          Gender:<input type="radio" name="gender" value="Male" checked={gender === "Male"}
+              onChange={(event) => setGender(event.target.value)} /> Male
+            <input type="radio" name="gender" value="Female" checked={gender === "Female"}
+              onChange={(event) => setGender(event.target.value)} /> Female
+          </div>
         </label>
         <label className="form-label">
           Location:
